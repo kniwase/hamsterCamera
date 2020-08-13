a08de39262bfd0a6269c49e4d3c9652214dc0320
Kento Niwase <kento.niwase@gmail.com>
2020-08-13 15:59:33 +0900

    PWAを無効化

--- hamcam-views/src/main.js
+++ hamcam-views/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueCompAPI from "@vue/composition-api";
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
